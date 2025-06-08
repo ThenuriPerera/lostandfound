dea13a7febb075659076e0c5cfae23a95dec9d8c
thenunp <thenunp@gmail.com>
2025-06-09 01:08:13 +0530

    first commit

--- src/main/java/com/example/lostandfound/LostandfoundApplication.java
+++ src/main/java/com/example/lostandfound/LostandfoundApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LostandfoundApplication {
 
 	public static void main(String[] args) {
+		System.out.println("helloo");
 		SpringApplication.run(LostandfoundApplication.class, args);
 	}
 
